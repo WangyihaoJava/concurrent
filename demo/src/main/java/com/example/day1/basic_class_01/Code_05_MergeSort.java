c8dd4d3451fbecca28ef90fadaea5ed0210a9fca
“WangyihaoJava” <WangyihaoJava@qq.com>
2021-12-02 22:25:12 +0800

    增加注释

--- demo/src/main/java/com/example/day1/basic_class_01/Code_05_MergeSort.java
+++ demo/src/main/java/com/example/day1/basic_class_01/Code_05_MergeSort.java
@@ -2,6 +2,14 @@ package com.example.day1.basic_class_01;
 
 import java.util.Arrays;
 
+
+/**
+ * 归并排序的过程就是先划分成两部分 左边和右边
+ * 左边一直划分 直到划分到剩一个数返回 和下一个递归过程返回的数 进行外排合并 此时外排合并就能保证在这两个数的范围上有序 然后拷贝到原数组
+ * 这样一直递归一直外排 依次 外排的范围越来越大 直到整个数组分为两个部分进行最后依次外排 完成排序 注意每次外排都是保证当前范围内的有序 并且之前已经排好的不用重新再排
+ *
+ * 时间复杂度 两个递归行为加上合并的行为 2 * n/2 + n
+ */
 public class Code_05_MergeSort {
 
 	public static void mergeSort(int[] arr) {
@@ -21,23 +29,33 @@ public class Code_05_MergeSort {
 		mergeSort(arr, mid + 1, r);
 		//最后把两部分进行合并
 		merge(arr, l, mid, r);
+
+
 	}
 
 
+	//合并的过程就是拿着左边第一个和右边第一个进行比较 谁小要哪个放进额外数组
+	//最后把这个额外数组拷贝进去
 	public static void merge(int[] arr, int l, int m, int r) {
 		int[] help = new int[r - l + 1];
 		int i = 0;
 		int p1 = l;
 		int p2 = m + 1;
+
+		//两个都不越界的情况下 拿着左边第一个和右边第一个进行比较 谁小要哪个放进额外数组
 		while (p1 <= m && p2 <= r) {
 			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
 		}
+
+		//两个必要且只有一个越界 把没越界的放进去
 		while (p1 <= m) {
 			help[i++] = arr[p1++];
 		}
 		while (p2 <= r) {
 			help[i++] = arr[p2++];
 		}
+
+		//拷贝到原数组
 		for (i = 0; i < help.length; i++) {
 			arr[l + i] = help[i];
 		}
@@ -102,8 +120,8 @@ public class Code_05_MergeSort {
 	// for test
 	public static void main(String[] args) {
 		int testTime = 500000;
-		int maxSize = 100;
-		int maxValue = 100;
+		int maxSize = 10;
+		int maxValue = 10;
 		boolean succeed = true;
 		for (int i = 0; i < testTime; i++) {
 			int[] arr1 = generateRandomArray(maxSize, maxValue);
@@ -123,6 +141,20 @@ public class Code_05_MergeSort {
 		printArray(arr);
 		mergeSort(arr);
 		printArray(arr);
+		int[]tt = new int[]{3,5,7,1,0};
+		System.out.println(mergeSortT(tt,0,tt.length - 1));
+
+	}
+
+	public static int mergeSortT(int[] arr, int l, int r) {
+		if (l == r) {
+			return arr[l];
+		}
+		int mid = l + ((r - l) >> 1);
+		//把数组分成两半 递归分别对两边排序
+		int maxleft = mergeSortT(arr, l, mid);
+		int maxright = mergeSortT(arr, mid + 1, r);
+		return Math.max(maxleft,maxright);
 
 	}
 
